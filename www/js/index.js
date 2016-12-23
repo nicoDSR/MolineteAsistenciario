831c97142e19265526fb1d986c17b0db7e0d5012
nicoDSR <nico.uy@gmail.com>
2016-12-23 12:22:53 -0300

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -125,7 +125,6 @@ inicializar :function(hostserver){
             deviceType = (navigator.userAgent.match(/iPad/i))  == "iPad" ? "iPad" : (navigator.userAgent.match(/iPhone/i))  == "iPhone" ? "iPhone" : (navigator.userAgent.match(/Android/i)) == "Android" ? "Android" : (navigator.userAgent.match(/BlackBerry/i)) ==  "BlackBerry" ? "BlackBerry" : "null";
             pictureSource = navigator.camera.PictureSourceType;
             destinationType = navigator.camera.DestinationType;
-            server = host;
             asist.loadClases();  
         });
     }); 
